7a38a83ff04d4a3a60ed5eda60429e4a10a6e1e4
tah-ir-787 <151051667+tah-ir-787@users.noreply.github.com>
2024-03-27 02:41:22 +0500

    i have complete number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("welcome to game - number_gussing_game00");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulation ! you guess a correct number.");
+}
+else {
+    console.log("Sorry, you guess awrong number");
+}
